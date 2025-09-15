a3eaca0e8ee2e7b051f610ac7b39600e8ec053de
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:48:20 -0400

    Add Lesson_10_Creation_And_Manipulation_Of_Data_Frames/part_1_create_data_frame.r

---
+++ Lesson_10_Creation_And_Manipulation_Of_Data_Frames/part_1_create_data_frame.r
@@ -0,0 +1,8 @@
+my_data_frame <- data.frame(
+  Name = c("Alice", "Bob", "Charlie", "Diana"),
+  Age = c(23, 35, 30, 29),
+  Height = c(158, 175, 180, 165),
+  Gender = factor(c("F", "M", "M", "F"))
+)
+
+print(my_data_frame)
