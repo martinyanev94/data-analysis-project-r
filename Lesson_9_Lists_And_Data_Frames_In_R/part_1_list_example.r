d77b83b4df9ad277262eda8544904486f550de7a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:55:22 -0400

    Add Lesson_9_Lists_And_Data_Frames_In_R/part_1_list_example.r

---
+++ Lesson_9_Lists_And_Data_Frames_In_R/part_1_list_example.r
@@ -0,0 +1,8 @@
+my_list <- list(
+  numbers = c(1, 2, 3, 4, 5),
+  characters = c("apple", "banana", "cherry"),
+  matrix_data = matrix(1:6, nrow = 2, ncol = 3),
+  logicals = c(TRUE, FALSE, TRUE)
+)
+
+print(my_list)
