77c6883dd67dcda19017dabeaf2589178c90e709
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:55:03 -0400

    Add Lesson_6_Advanced_Operations_With_Matrices_And_Arrays/part_4_array_manipulation.r

---
+++ Lesson_6_Advanced_Operations_With_Matrices_And_Arrays/part_4_array_manipulation.r
@@ -0,0 +1,6 @@
+# Creating a 3-dimensional array
+my_array <- array(1:24, dim=c(4, 3, 2))
+print(my_array)
+# Accessing specific elements from the array
+specific_row <- my_array[2, , 1]
+print(specific_row)
