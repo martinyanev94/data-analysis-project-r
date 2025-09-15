7013a0d29ee2917065b528eecfb6c8045af2206b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:55:06 -0400

    Add Lesson_7_Logical_Operators_In_R/part_1_logical_operations.r

---
+++ Lesson_7_Logical_Operators_In_R/part_1_logical_operations.r
@@ -0,0 +1,6 @@
+result <- TRUE & TRUE
+print(result)  # This will print [1] TRUE
+print(TRUE & FALSE)  # This will print [1] FALSE
+print(FALSE & FALSE) # This will print [1] FALSE
+result <- TRUE && FALSE
+print(result)  # This will print [1] FALSE
