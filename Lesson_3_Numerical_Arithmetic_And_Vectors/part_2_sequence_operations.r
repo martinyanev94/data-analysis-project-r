4b755b98a7f7fa38544c87305595c697beb8c05e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:53:00 -0400

    Add Lesson_3_Numerical_Arithmetic_And_Vectors/part_2_sequence_operations.r

---
+++ Lesson_3_Numerical_Arithmetic_And_Vectors/part_2_sequence_operations.r
@@ -0,0 +1,32 @@
+my_seq <- seq(1, 10)
+
+my_seq2 <- seq(from = 5, to = 20, by = 3)
+
+rep(1:3, times = 3)
+
+rep(1:3, each = 2)
+
+R
+my_seq <- seq(1, 10)
+
+
+[1] 1 2 3 4 5 6 7 8 9 10
+
+R
+my_seq2 <- seq(from = 5, to = 20, by = 3)
+
+
+[1]  5  8 11 14 17 20
+
+R
+rep(1:3, times = 3)
+
+
+[1] 1 2 3 1 2 3 1 2 3
+
+R
+rep(1:3, each = 2)
+
+
+[1] 1 1 2 2 3 3
+
