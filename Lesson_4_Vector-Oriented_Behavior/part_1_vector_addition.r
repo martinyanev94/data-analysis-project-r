22ad483ec180660e33acb2f5b6ec7b7d7691ff6d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:30 -0400

    Add Lesson_4_Vector-Oriented_Behavior/part_1_vector_addition.r

---
+++ Lesson_4_Vector-Oriented_Behavior/part_1_vector_addition.r
@@ -0,0 +1,12 @@
+vector_a <- c(10, 20, 30)
+vector_b <- c(1, 2, 3)
+result <- vector_a + vector_b
+
+R
+vector_a <- c(10, 20, 30)
+vector_b <- c(1, 2, 3)
+result <- vector_a + vector_b
+
+
+[1] 11 22 33
+
