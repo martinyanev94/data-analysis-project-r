72ceac96b4cd7b1fb90cc77f2eda13525ac9f598
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:51:10 -0400

    Add Lesson_27_Data_Visualization_Techniques/part_3_cylinder_distribution_pie_chart.r

---
+++ Lesson_27_Data_Visualization_Techniques/part_3_cylinder_distribution_pie_chart.r
@@ -0,0 +1,5 @@
+# Basic pie chart of cylinders
+pie(cyl_freq,
+    labels = c("4 Cylinders", "6 Cylinders", "8 Cylinders"),
+    col = c("yellow", "orange", "red"),
+    main = "Distribution of Cars by Cylinder")
