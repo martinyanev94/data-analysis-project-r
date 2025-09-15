ee300c66dc58c268864a708b12197d67dd0fe26a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:52:52 -0400

    Add Lesson_39_Overview_Of_Simple_Linear_Regression/part_3_survival_analysis_summary.r

---
+++ Lesson_39_Overview_Of_Simple_Linear_Regression/part_3_survival_analysis_summary.r
@@ -0,0 +1,17 @@
+summary(survfit)
+
+Estimate Std. Error t value Pr(>|t|)   
+(Intercept)    113.9536  5.4416   20.94   <2e-16 ***
+Wr.Hnd         3.1166    0.2888   10.79   <2e-16 ***
+R
+summary(survfit)
+
+
+Multiple R-squared:  0.3612,  Adjusted R-squared:  0.3581
+
+
+Coefficients:
+               Estimate Std. Error t value Pr(>|t|)   
+(Intercept)    113.9536  5.4416   20.94   <2e-16 ***
+Wr.Hnd         3.1166    0.2888   10.79   <2e-16 ***
+
