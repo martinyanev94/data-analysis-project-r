9f4ef7ada766261410f28f34ffc3a1b966f675a5
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:33 -0400

    Add Lesson_4_Vector-Oriented_Behavior/part_3_vector_recycling.r

---
+++ Lesson_4_Vector-Oriented_Behavior/part_3_vector_recycling.r
@@ -0,0 +1,12 @@
+short_vector <- c(1, 2)
+long_vector <- c(10, 20, 30, 40)
+result_recycling <- long_vector + short_vector
+
+R
+short_vector <- c(1, 2)
+long_vector <- c(10, 20, 30, 40)
+result_recycling <- long_vector + short_vector
+
+
+[1] 11 22 32 42
+
