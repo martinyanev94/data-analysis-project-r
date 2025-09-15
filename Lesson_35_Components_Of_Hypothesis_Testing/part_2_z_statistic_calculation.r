3c70c983e8896494e0b0d9b308b467077c395267
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:52:23 -0400

    Add Lesson_35_Components_Of_Hypothesis_Testing/part_2_z_statistic_calculation.r

---
+++ Lesson_35_Components_Of_Hypothesis_Testing/part_2_z_statistic_calculation.r
@@ -0,0 +1,8 @@
+# Sample statistics
+sample_mean <- 120
+population_sd <- 15
+sample_size <- 30
+
+# Calculate z-statistic
+z_statistic <- (sample_mean - 130) / (population_sd / sqrt(sample_size))
+z_statistic
