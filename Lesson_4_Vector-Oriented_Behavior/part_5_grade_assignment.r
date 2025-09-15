eb50973de663b3f240c1ecdb5a9c2d083e046d99
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:36 -0400

    Add Lesson_4_Vector-Oriented_Behavior/part_5_grade_assignment.r

---
+++ Lesson_4_Vector-Oriented_Behavior/part_5_grade_assignment.r
@@ -0,0 +1,10 @@
+scores <- c(82, 91, 76, 45, 67)
+grades <- ifelse(scores >= 75, "Pass", "Fail")
+
+R
+scores <- c(82, 91, 76, 45, 67)
+grades <- ifelse(scores >= 75, "Pass", "Fail")
+
+
+[1] "Pass" "Pass" "Pass" "Fail" "Fail"
+
