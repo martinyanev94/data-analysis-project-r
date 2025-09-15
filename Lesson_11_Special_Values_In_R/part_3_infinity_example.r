e298b95e3ea7e91032c9e5d34818f90b949ed707
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:48:37 -0400

    Add Lesson_11_Special_Values_In_R/part_3_infinity_example.r

---
+++ Lesson_11_Special_Values_In_R/part_3_infinity_example.r
@@ -0,0 +1,5 @@
+# Example of Inf and -Inf
+positive_infinity <- 1/0  # Inf
+negative_infinity <- -1/0  # -Inf
+is_pos_inf <- is.infinite(positive_infinity)  # TRUE
+is_neg_inf <- is.infinite(negative_infinity)  # TRUE
