3e1974ce6fda5e9e5f04d296d476a809fb08fe1d
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:48:41 -0400

    Add Lesson_11_Special_Values_In_R/part_6_empty_vs_whitespace.r

---
+++ Lesson_11_Special_Values_In_R/part_6_empty_vs_whitespace.r
@@ -0,0 +1,6 @@
+# Example of empty strings vs. whitespace
+empty_string <- ""
+whitespace_string <- " "
+
+is_empty <- nchar(empty_string) == 0  # TRUE
+is_whitespace <- nchar(whitespace_string) == 0  # FALSE
