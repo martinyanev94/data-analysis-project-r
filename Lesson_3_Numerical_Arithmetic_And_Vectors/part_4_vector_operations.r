8d1fb3dcaaecbd1ce7ef5305d9d985b164186de6
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:53:03 -0400

    Add Lesson_3_Numerical_Arithmetic_And_Vectors/part_4_vector_operations.r

---
+++ Lesson_3_Numerical_Arithmetic_And_Vectors/part_4_vector_operations.r
@@ -0,0 +1,20 @@
+length(foo)
+
+foo <- c(1, 2, 3, 4)
+other_vec <- c(5, 6, 7, 8)
+foo + other_vec
+
+R
+length(foo)
+
+
+[1] 6
+
+R
+foo <- c(1, 2, 3, 4)
+other_vec <- c(5, 6, 7, 8)
+foo + other_vec
+
+
+[1]  6  8 10 12
+
