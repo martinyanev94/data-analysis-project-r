0cf285c608d92a935a6c5c272b03609b9d55a78c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:35 -0400

    Add Lesson_4_Vector-Oriented_Behavior/part_4_extract_even_numbers.r

---
+++ Lesson_4_Vector-Oriented_Behavior/part_4_extract_even_numbers.r
@@ -0,0 +1,10 @@
+mixed_numbers <- c(1, 2, 3, 4, 5, 6)
+even_numbers <- mixed_numbers[mixed_numbers %% 2 == 0]
+
+R
+mixed_numbers <- c(1, 2, 3, 4, 5, 6)
+even_numbers <- mixed_numbers[mixed_numbers %% 2 == 0]
+
+
+[1] 2 4 6
+
