ae218fa54cf0f4ab069a63b2aa58de1111909448
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:50:04 -0400

    Add Lesson_1_Basics_Of_R_Programming/part_6_vector_operations.r

---
+++ Lesson_1_Basics_Of_R_Programming/part_6_vector_operations.r
@@ -0,0 +1,24 @@
+my_vector <- c(4, 5, 6, 7)
+
+my_vector * 2
+
+mean(my_vector)
+
+R
+my_vector <- c(4, 5, 6, 7)
+
+
+[1] 4 5 6 7
+
+R
+my_vector * 2
+
+
+[1] 8 10 12 14
+
+R
+mean(my_vector)
+
+
+[1] 5.5
+
