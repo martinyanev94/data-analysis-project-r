71c0c0d6276bfe989d4f2702f46279c31fee5a08
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:48:44 -0400

    Add Lesson_12_Object_Classes_And_Coercion_In_R/part_1_data_conversion.r

---
+++ Lesson_12_Object_Classes_And_Coercion_In_R/part_1_data_conversion.r
@@ -0,0 +1,6 @@
+my_num <- 5
+my_char <- " apples"
+result <- paste(my_num, my_char)  # This will convert my_num to character
+my_numeric_vector <- c(1.8, 2.0, 3.5)
+my_integer_vector <- as.integer(my_numeric_vector)  
+print(my_integer_vector)  # Output will be 1, 2, 3
