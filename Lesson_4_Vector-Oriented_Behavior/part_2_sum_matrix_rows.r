8feeb03f4d477ce7fc0e3c89515b236145dedf37
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:32 -0400

    Add Lesson_4_Vector-Oriented_Behavior/part_2_sum_matrix_rows.r

---
+++ Lesson_4_Vector-Oriented_Behavior/part_2_sum_matrix_rows.r
@@ -0,0 +1,10 @@
+matrix_data <- matrix(1:9, nrow=3)
+result_apply <- apply(matrix_data, 1, sum)  # Summing across rows
+
+R
+matrix_data <- matrix(1:9, nrow=3)
+result_apply <- apply(matrix_data, 1, sum)  # Summing across rows
+
+
+[1] 12 15 18
+
