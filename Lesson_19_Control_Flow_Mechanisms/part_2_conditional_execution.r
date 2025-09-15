81b372c07488260e98e77c6f7e47dc6cd3df3f1c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:49:47 -0400

    Add Lesson_19_Control_Flow_Mechanisms/part_2_conditional_execution.r

---
+++ Lesson_19_Control_Flow_Mechanisms/part_2_conditional_execution.r
@@ -0,0 +1,12 @@
+if (condition) {
+    # code to execute if condition is TRUE
+}
+x <- 10
+if (x > 5) {
+    print("x is greater than 5!")
+}
+if (x > 5) {
+    print("x is greater than 5!")
+} else {
+    print("x is 5 or less.")
+}
