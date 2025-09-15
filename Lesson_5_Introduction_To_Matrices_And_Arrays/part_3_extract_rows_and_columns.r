24339233ca29f14c12c794554bc5a6a1d8e3541a
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:51 -0400

    Add Lesson_5_Introduction_To_Matrices_And_Arrays/part_3_extract_rows_and_columns.r

---
+++ Lesson_5_Introduction_To_Matrices_And_Arrays/part_3_extract_rows_and_columns.r
@@ -0,0 +1,34 @@
+# Extracting the first row
+first_row <- A[1, ]
+print(first_row)
+
+# Extracting the second column
+second_column <- A[, 2]
+print(second_column)
+
+R
+# Extracting the first row
+first_row <- A[1, ]
+print(first_row)
+
+
+[1] 1 3 5
+
+
+
+[1] 1 3 5
+
+
+And if you want to extract the second column, it can be done as follows:
+
+R
+# Extracting the second column
+second_column <- A[, 2]
+print(second_column)
+
+
+The output will be:
+
+
+[1] 3 4
+
