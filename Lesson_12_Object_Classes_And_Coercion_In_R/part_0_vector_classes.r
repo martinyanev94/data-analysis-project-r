fafbef6d673cf04787acb703717c1ee6ea6f5660
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:48:43 -0400

    Add Lesson_12_Object_Classes_And_Coercion_In_R/part_0_vector_classes.r

---
+++ Lesson_12_Object_Classes_And_Coercion_In_R/part_0_vector_classes.r
@@ -0,0 +1,4 @@
+my_numeric_vector <- c(1.5, 2.3, 3.8)
+class(my_numeric_vector)  # This will return "numeric"
+my_character_vector <- c("apple", "banana", "cherry")
+class(my_character_vector)  # This will return "character"
