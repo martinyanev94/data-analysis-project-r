b57cc9ceaf81d48e8e831ce8b71b76f13de3c183
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:51:21 -0400

    Add Lesson_28_Advanced_Scatterplot_Analysis/part_1_hp_vs_mpg_scatterplot.r

---
+++ Lesson_28_Advanced_Scatterplot_Analysis/part_1_hp_vs_mpg_scatterplot.r
@@ -0,0 +1,7 @@
+# Basic scatterplot of hp and mpg
+plot(mtcars$hp, mtcars$mpg,
+     main = "Horsepower vs. MPG",
+     xlab = "Horsepower",
+     ylab = "Miles Per Gallon",
+     pch = 19,
+     col = "blue")
