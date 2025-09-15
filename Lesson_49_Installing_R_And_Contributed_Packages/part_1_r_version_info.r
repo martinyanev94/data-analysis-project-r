0dac07a5c1a5df1f98243734b78af0eba2f9e364
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:54:20 -0400

    Add Lesson_49_Installing_R_And_Contributed_Packages/part_1_r_version_info.r

---
+++ Lesson_49_Installing_R_And_Contributed_Packages/part_1_r_version_info.r
@@ -0,0 +1,4 @@
+R version 4.1.1 (2021-08-10)
+Copyright (C) 2021 The R Foundation for Statistical Computing
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+Type 'citation()' for citing R in publications.
