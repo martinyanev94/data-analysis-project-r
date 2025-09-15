27b9023520308c5fe88432735423af4746b5ecf2
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-15 06:49:49 -0400

    Add Lesson_19_Control_Flow_Mechanisms/part_3_conditional_statements.r

---
+++ Lesson_19_Control_Flow_Mechanisms/part_3_conditional_statements.r
@@ -0,0 +1,14 @@
+if (condition1) {
+    # code to execute if condition1 is TRUE
+} else if (condition2) {
+    # code to execute if condition2 is TRUE
+} else {
+    # code to execute if neither condition is TRUE
+}
+if (x > 10) {
+    print("x is greater than 10.")
+} else if (x == 10) {
+    print("x is exactly 10.")
+} else {
+    print("x is less than 10.")
+}
